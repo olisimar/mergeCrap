f168892d5217674fd56e100771bc0da7d139f25b
Werner Johansson <kwm.johansson@gmail.com>
2015-11-02 13:05:28 +0100

    Create a problem

--- src/main/java/se/yrgo/java15/mergeCrap/MergeTrouble.java
+++ src/main/java/se/yrgo/java15/mergeCrap/MergeTrouble.java
@@ -3,6 +3,7 @@ package se.yrgo.java15.mergeCrap;
 public class MergeTrouble {
 
 	public static void main(String[] args) {
-		System.out.println("This file is to show merge conflicts.");
+		System.out.println("This file is to "
+			+"show merge conflicts.");
 	}
 }

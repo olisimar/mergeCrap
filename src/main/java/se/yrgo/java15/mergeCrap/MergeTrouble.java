3b8e8ee0b1ac689a571a7ed937f6548643144cbc
Werner Johansson <kwm.johansson@gmail.com>
2015-11-02 08:53:04 +0100

    Setting up again.

--- src/main/java/se/yrgo/java15/mergeCrap/MergeTrouble.java
+++ src/main/java/se/yrgo/java15/mergeCrap/MergeTrouble.java
@@ -3,7 +3,6 @@ package se.yrgo.java15.mergeCrap;
 public class MergeTrouble {
 
 	public static void main(String[] args) {
-		System.out.println("Adding content here yo!");
 		System.out.println("This file is to show merge conflicts.");
 	}
 }

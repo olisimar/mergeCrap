71b210320cd57a1321db9dbe35e5eea126c82f00
Werner Johansson <kwm.johansson@gmail.com>
2015-11-02 08:50:30 +0100

    Setting our selves up for trouble.

--- src/main/java/se/yrgo/java15/mergeCrap/MergeTrouble.java
+++ src/main/java/se/yrgo/java15/mergeCrap/MergeTrouble.java
@@ -5,5 +5,6 @@ public class MergeTrouble {
 	public static void main(String[] args) {
 		System.out.println("This file is to "
 			+"show merge conflicts.");
+		System.out.println("Adding content here yo!");
 	}
 }
